5f10e1f3fa414f571e563593da0c2ad2c9562d9c
mriskyamin94 <rizky04@users.noreply.github.com>
2021-06-11 15:48:51 +0700

    eccomerce

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:ecommerce/screen/menu.dart';
+import 'package:flutter/material.dart';
+// ignore: duplicate_import
+import 'package:ecommerce/screen/menu.dart';
+
+void main() => runApp(MaterialApp(
+      home: Menu(),
+      debugShowCheckedModeBanner: false,
+    ));
